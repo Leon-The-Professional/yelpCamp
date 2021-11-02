ef7573c1b7fb06d8c0b2c72ff6a84f229fb3ca15
Leon Hall <leonus.hall@gmail.com>
2021-11-02 06:31:12 -0600

    add login and logout functionality

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,9 @@
 const { builtinModules } = require('module');
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+const Review = require('./review');
+
+
 
 const CampgroundSchema = new Schema({
     title: {
@@ -18,12 +21,26 @@ const CampgroundSchema = new Schema({
     location: {
         type: String,
     },
-    review: [
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Review'
         }
     ]
+})
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
