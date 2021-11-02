aa67f827eb9116a4c9ebc39d5fbbb5113814574b
Leon Hall <leonus.hall@gmail.com>
2021-11-02 07:57:07 -0600

    add authentication middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,20 +2,10 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
 const { campgroundSchema } = require('../schemas')
-const { isLoggedIn } = require('../middleware')
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 
-const ExpressError = require('../utils/ExpressError')
 const Campground = require('../models/campground');
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    };
-}
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({})
@@ -28,13 +18,14 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground)
+    campground.author = req.user._id;
     await campground.save()
     req.flash('success', 'succesfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews')
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author')
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         res.redirect('/campgrounds')
@@ -42,8 +33,9 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render('campgrounds/show', { campground })
 }))
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         res.redirect('/campgrounds')
@@ -51,10 +43,10 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
-    res.flash('success', 'Successfully updated Campground!')
+    req.flash('success', 'Successfully updated Campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDb = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state} `,
             title: `${sample(descriptors)} ${sample(places)}`,
+            author: '61808a38d46e2de59f631e43',
             image: 'https://source.unsplash.com/collection/483251/',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam libero mollitia neque facere.Nihil voluptatem pariatur facere dolor deleniti ut quasi ex eius cum sunt perferendis, quaerat hic quia assumenda possimus molestias, aliquam a officiis eveniet, harum tempore ratione excepturi? Unde voluptatibus ab cum non veniam quam, tenetur fugit est.',
             price
